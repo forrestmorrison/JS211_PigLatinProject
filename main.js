d96749bc157a4448e43ee79584f4d37983836b28
forrestmorrison <forrestmorrison@gmail.com>
2022-01-18 10:37:36 -0600

    eattlesay

--- main.js
+++ main.js
@@ -5,26 +5,50 @@ const assert = require('assert');
 // brings in the readline module to access the command line
 const readline = require('readline');
 // use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 
 const pigLatin = (word) => {
 
-  // Your code here
+// input: Seattle   output: eattlesay
+// input: Austin   output: austinyay
+
+let vowels = ["a", "e", "i", "o", "u"];
+let wordArray = word.toLowerCase().split("");
+
+// if (vowels.includes(wordArray[0])) {
+//   return wordArray.join("") + "way";
+// }
+
+let firstCons = [];
+let latinWord = "";
+
+for (let i = 0; i < wordArray.length; i++) {
+  if (vowels.includes(wordArray[i])) {
+    break;
+  }
+  if (!vowels.includes(wordArray[i])) {
+    firstCons.push(wordArray[i]);
+  }
+}
+
+return wordArray.slice(firstCons.length).join("") + firstCons + "ay";
 
 }
+
+console.log(pigLatin("Seattle"));
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
 const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
+//  rl.question('word ', (answer) => {
+//    console.log( pigLatin(answer) );
+//    getPrompt();
+//  });
 }
 
 // Unit Tests
