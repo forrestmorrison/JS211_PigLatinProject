3b4cb83f68e91e51e9d3064294d0382a5e773878
forrestmorrison <forrestmorrison@gmail.com>
2022-01-19 16:36:13 -0600

    working Pig Latin function passing all tests

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const assert = require('assert');
 // brings in the readline module to access the command line
 const readline = require('readline');
 // use the readline module to print out to the command line
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
 const pigLatin = (word) => {
@@ -39,16 +39,14 @@ return wordArray.slice(firstCons.length).join("") + firstCons.join("") + latinWo
 
 }
 
-console.log(pigLatin("Chicago"));
-
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
 const getPrompt = () => {
-//  rl.question('word ', (answer) => {
-//    console.log( pigLatin(answer) );
-//    getPrompt();
-//  });
+ rl.question('word ', (answer) => {
+   console.log( pigLatin(answer) );
+   getPrompt();
+ });
 }
 
 // Unit Tests
